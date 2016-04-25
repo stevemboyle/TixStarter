b0edab0f5cd8fd5d704162932569ddeb7a6aa3ab
Steve Boyle <stevemboyle@Steves-MacBook-Air.local>
2016-04-25 14:53:17 -0700

    adding potential seed data

---
+++ Initial-Proposal-Documents/Potential-Seed-Data/potential_users_seed_data.rb
@@ -0,0 +1,16 @@
+
+User.create({
+        username: linmanuel,
+        first_name: "Lin-Manuel",
+        last_name: "Miranda",
+        bio_image_url: ,
+        bio_text:
+        })
+
+User.create({
+        username: hushhuhs,
+        first_name: "Felix",
+        last_name: "Barrett",
+        bio_image_url: ,
+        bio_text:
+        })
