5712937d9dbe4186d722c32c31d04f1d3ef78b35
Steve Boyle <stevemboyle@Steves-MacBook-Air.local>
2016-04-26 13:50:26 -0700

    adding demo modal

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,25 +1,48 @@
 var React = require('react');
 var EventForm = require('./events/form');
 var EventIndex = require('./events/index');
-var LoginForm = require('./users/usersLoginForm');
+// var LoginForm = require('./users/usersLoginForm');
+var Modal = require("react-modal");
 
 //Mixins
-var CurrentUserState = require('.././mixins/currentUserState');
+// var CurrentUserState = require('.././mixins/currentUserState');
 
 module.exports = React.createClass({
 
-  mixins: [CurrentUserState],
+  // mixins: [CurrentUserState],
+
+  getInitialState: function(){
+     return({ modalOpen: false });
+   },
+
+   openModal: function(){
+     this.setState({ modalOpen: true });
+   },
+
+   closeModal: function(){
+     this.setState({ modalOpen: false });
+   },
 
   render: function () {
 
     return(
 
-
-
       <div>
+
         <div id="othermenu">
           <p>hello</p>
+          <button onClick={this.openModal}>Open Me!</button>
         </div>
+
+        <Modal
+          isOpen={this.state.modalOpen}
+          onRequestClose={this.closeModal}>
+
+            <h2>Im a modal!</h2>
+            <p>modal modal modal modal modal</p>
+            <p>mooooooooodal!</p>
+
+        </Modal>
 
         <div>
 
